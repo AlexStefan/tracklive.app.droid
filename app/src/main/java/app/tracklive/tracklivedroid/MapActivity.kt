4e8dc3a0aab81f2deef27abdc5acb93c967ee146
NiculescuB <31341111+NiculescuB@users.noreply.github.com>
2018-08-31 15:25:14 +0300

    create Droid project
    First ocmmit with Droid project

---
+++ app/src/main/java/app/tracklive/tracklivedroid/MapActivity.kt
@@ -0,0 +1,56 @@
+package app.tracklive.tracklivedroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener  {
+
+    override fun onMarkerClick(p0: Marker?) = false
+
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        // TODO: make fusedLocationClient work, and then uncomment
+        // fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val ortecRo = LatLng(44.428586, 26.052446)
+        map.addMarker(MarkerOptions().position(ortecRo).title("We are watching you"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(ortecRo))
+
+        map.getUiSettings().setZoomControlsEnabled(true)
+        map.setOnMarkerClickListener(this)
+
+    }
+}
